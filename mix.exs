e461d49d1dfa1f6d5c32e06c26f668cf241a12c9
techno-tanoC <develop.tanoc@gmail.com>
2017-02-04 15:46:10 +0900

    write the meta info

--- mix.exs
+++ mix.exs
@@ -2,12 +2,20 @@ defmodule Func.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :func,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :func,
+      version: "0.1.0",
+      elixir: "~> 1.4",
+      description: "Functions for functional programming.",
+      package: [
+        maintainers: ["techno-tanoC"],
+        licenses: ["MIT"],
+        links: %{"GitHub" => "https://github.com/techno-tanoC/func"}
+      ],
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps()
+    ]
   end
 
   # Configuration for the OTP application
@@ -15,7 +23,11 @@ defmodule Func.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger]]
+    [
+      extra_applications: [
+        :logger
+      ]
+    ]
   end
 
   # Dependencies can be Hex packages:
@@ -28,6 +40,8 @@ defmodule Func.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:ex_doc, "~> 0.14", only: :dev},
+    ]
   end
 end
