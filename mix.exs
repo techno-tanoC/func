5c000b9ff87f4dc3b294702b1f00b7017ba17054
techno-tanoC <develop.tanoc@gmail.com>
2017-02-04 17:07:04 +0900

    version 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Func.Mixfile do
   def project do
     [
       app: :func,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.4",
       description: "Functions for functional programming.",
       package: [
