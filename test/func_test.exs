c63886e14c33741b25e66f6db98a685d959cf767
techno-tanoC <develop.tanoc@gmail.com>
2017-02-04 16:23:27 +0900

    add test: id, const, run, curry, flip, curry_flip, compose

--- test/func_test.exs
+++ test/func_test.exs
@@ -1,8 +1,33 @@
-defmodule FuncTest do
+defmodule FuncFunctionTest do
   use ExUnit.Case
-  doctest Func
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  import Func.Function
+
+  test "id" do
+    assert id().(1) == 1
+  end
+
+  test "const" do
+    assert const(1).(2) == 1
+  end
+
+  test "run" do
+    assert run(1).() == 1
+  end
+
+  test "curry" do
+    assert curry(&+/2).(1).(2) == 3
+  end
+
+  test "flip" do
+    assert flip(&-/2).(1, 2) == 1
+  end
+
+  test "curry_flip" do
+    assert curry_flip(&-/2).(1).(2) == 1
+  end
+
+  test "compose" do
+    assert compose(&String.to_integer/1, &Integer.to_string/1).(1) == 1
   end
 end
