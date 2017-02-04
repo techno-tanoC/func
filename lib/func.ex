f6ba18ca68410a15418def3d57d8527d1901f22f
techno-tanoC <develop.tanoc@gmail.com>
2017-02-04 15:40:56 +0900

    first commit

---
+++ lib/func.ex
@@ -0,0 +1,18 @@
+defmodule Func do
+  @moduledoc """
+  Documentation for Func.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Func.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/func_test.exs
@@ -0,0 +1,8 @@
+defmodule FuncTest do
+  use ExUnit.Case
+  doctest Func
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
