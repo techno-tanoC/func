f59c5c65ec11182e715bf12f422421a65fbd7338
techno-tanoC <develop.tanoc@gmail.com>
2017-02-11 00:32:52 +0900

    change curry and uncurry

--- lib/func/function.ex
+++ lib/func/function.ex
@@ -38,28 +38,51 @@ defmodule Func.Function do
   def always(val), do: fn -> val end
 
   @doc """
-  Curry function.
+  Composition function. Compose functions.
 
-      iex> Func.Function.curry(&+/2).(1).(2)
-      3
-
-  """
-  def curry(func) do
-    fn a ->
-      fn b ->
-        func.(a, b)
-      end
-    end
-  end
-
-  @doc """
-  Uncurry function.
-
-      iex> Func.Function.uncurry(fn a -> fn b -> a - b end end).(2, 1)
+      iex> Func.Function.compose(&String.to_integer/1, &Integer.to_string/1).(1)
       1
 
   """
-  def uncurry(func), do: fn a, b -> func.(a).(b) end
+  def compose(f, g), do: fn x -> f.(g.(x)) end
+
+  @doc ~S"""
+
+      iex> Func.Function.curry(&(&1 + &2)).(1).(2)
+      3
+      iex> Func.Function.curry(&(&1 + &2 + &3)).(1).(2).(3)
+      6
+
+  """
+  def curry(fun) do
+    {_, arity} = :erlang.fun_info(fun, :arity)
+    curry(fun, arity, [])
+  end
+
+  defp curry(fun, 0, args), do: apply(fun, Enum.reverse(args))
+  defp curry(fun, arity, args) do
+    fn arg -> curry(fun, arity - 1, [arg | args]) end
+  end
+
+  @doc ~S"""
+
+      iex> Func.Function.uncurry(fn x -> fn y -> x + y end end).(1, 2)
+      3
+
+  """
+  def uncurry(fun), do: &(fun.(&1).(&2))
+
+  @doc ~S"""
+  Applys arguments to curried function.
+
+      iex> curried = fn x -> (fn y -> (fn z -> x + y + z end) end) end
+      ...> Func.Function.uncurry(curried, [1, 2, 3])
+      6
+
+  """
+  def uncurry(fun, args) when is_list(args) do
+    Enum.reduce(args, fun, &(&2).(&1))
+  end
 
   @doc """
   Flip function.
@@ -78,19 +101,6 @@ defmodule Func.Function do
 
   """
   def curry_flip(func) do
-    fn a ->
-      fn b ->
-        func.(b, a)
-      end
-    end
+    compose(&curry/1, &flip/1).(func)
   end
-
-  @doc """
-  Composition function. Compose functions.
-
-      iex> Func.Function.compose(&String.to_integer/1, &Integer.to_string/1).(1)
-      1
-
-  """
-  def compose(f, g), do: fn x -> f.(g.(x)) end
 end
