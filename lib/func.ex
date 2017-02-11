6a188d14d6c850d10141bea6ef82bfe5f1ab971e
techno-tanoC <develop.tanoc@gmail.com>
2017-02-11 16:05:25 +0900

    add directives

--- lib/func.ex
+++ lib/func.ex
@@ -1,7 +1,15 @@
 defmodule Func do
+  @moduledoc ~S"""
+  """
+
   defmacro __using__(_opts) do
     quote do
       import Func.Function
+      import Func.Tuple
+      import Func.Maybe
+      import Func.Num
+      import Func.Presence
+      import Func.Bool
     end
   end
 end
